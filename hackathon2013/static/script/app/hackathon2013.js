8d6490ab406c89f4a1c4e478a7eac5019e5d1617
Philipp Bender <philipp@rootiniert.de>
2013-06-08 17:15:41 +0200

    name and room required now

--- hackathon2013/static/script/app/hackathon2013.js
+++ hackathon2013/static/script/app/hackathon2013.js
@@ -8,6 +8,16 @@ var bnd = function(model, template) {
     ko.cleanNode(element);
     ko.applyBindings({'name': template, 'data': model}, element);
 };
+/* Gravity */
+
+var sammy_app = null;
+
+
+var bnd = function(model, template) {
+    var element = document.getElementById('page');
+    ko.cleanNode(element);
+    ko.applyBindings({'name': template, 'data': model}, element);
+};
 
 
 var assertHas = function(obj, property) {
@@ -57,6 +67,15 @@ var RoomListVM = function() {
           self.errors.push("Bitte wähle eine Gruppe aus");
         }
 
+        console.log("#errors: ", self.errors().length )
+
+        if( self.errors().length )
+        {
+            return;
+        }
+
+        console.log(self.errors(), " <- username" )
+
         sammy_app.setLocation('/room/'+self.selectedGroup().id()+'?user='+self.loginName());
     };
 };
@@ -102,109 +121,3 @@ $(document).ready(function() {
     // sammy_app.log = function() {};
     sammy_app.run();
 });
-
-/*
-Hackathon = Ember.Application.create();
-
-
-Hackathon.Event = DS.Model.extend({
-  id: DS.attr('string'),
-  user: DS.hasMany('Hackathon.User'),
-  tag: DS.hasMany('Hackathon.Tag')
-});
-
-
-Hackathon.Room = DS.Model.extend({
-  name: DS.attr('string'),
-  users: DS.hasMany('Hackathon.User')
-});
-
-
-Hackathon.User = DS.Model.extend({
-  name: DS.attr('string'),
-  tags: DS.hasMany('Hackathon.Tag'),
-  room: DS.belongsTo('Hackathon.Room')
-});
-
-
-Hackathon.Tag = DS.Model.extend({
-  label: DS.attr('string')
-});
-
-
-
-Hackathon.Store = DS.Store.extend({
-  revision: 12,
-  adapter: DS.DjangoRESTAdapter.create({
-      namespace: 'Hackathon'
-  })
-});
-
-
-
-Hackathon.RoomsView = Ember.View.extend({
-  addRoom: function(event) {
-    var name = this.get('name');
-    if (!name.trim()) {
-      alert("Please enter a name");
-    }
-    // var logged_in_user = this.get('logged_in_user');
-    // if (!logged_in_user.trim()) {
-    //   alert("Who are you?");
-    // }
-    var new_room = Hackathon.Room.createRecord({'name': name});
-    this.get('controller').addRoom(new_room);
-    this.set('name', '');
-  }
-});
-
-
-Hackathon.RoomsController = Ember.ArrayController.extend({
-  addRoom: function(room) {
-    this.get('store').commit();
-  }
-});
-
-
-
-Hackathon.RoomView = Ember.View.extend({
-  addUser: function(event) {
-    var name = this.get('name');
-    if (!name.trim()) {
-      alert("Please enter a name");
-    }
-    var new_user = Hackathon.User.createRecord({'name': name});
-    this.get('controller').addUser(new_user);
-    this.set('name', '');
-  }
-});
-
-
-Hackathon.RoomController = Ember.ObjectController.extend({
-  addUser: function(room) {
-    this.get('store').commit();
-  }
-});
-
-Hackathon.Router = Ember.Router.extend({
-  enableLogging: true,
-  location: 'history'
-});
-Hackathon.Router.map(function() {
-  this.route("rooms", {path : "/"});
-  this.route("room", { path : "/room/:room_id" });
-  this.route("user", { path : "/user/:user_id" });
-});
-
-Hackathon.RoomsRoute = Ember.Route.extend({
-  model: function() {
-    return Hackathon.Room.find();
-  }
-});
-Hackathon.RoomRoute = Ember.Route.extend({
-  model: function(options) {
-    Hackathon.User.find({'room': options.room_id});
-    return Hackathon.Room.find(options.room_id);
-  }
-});
-*/
