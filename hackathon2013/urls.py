6247655c79465c8f741b623595692328b237fcef
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-06-09 11:11:53 +0200

    Add static urls

--- hackathon2013/urls.py
+++ hackathon2013/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from hackathon2013.gravity.views import HomeView
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^api', include('hackathon2013.gravity.urls', namespace='hackathon')),
     url(r'^$', HomeView.as_view()),
